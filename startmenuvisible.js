8299a7113312b6b11bcf8a357b1567fcd0124a25
GalacticUnblockerOfficial <160625207+GalacticUnblockingCO@users.noreply.github.com>
2024-03-18 17:44:50 -0400

    Create startmenuvisible.js

---
+++ startmenuvisible.js
@@ -0,0 +1,8 @@
+// Function to display the start menu
+function displayStartMenu() {
+    // Get reference to the start menu
+    const startMenu = document.getElementById('startMenu');
+
+    // Display the start menu
+    startMenu.style.display = 'block';
+}
